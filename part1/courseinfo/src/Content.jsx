957678bdfce212118826f80cd8a438520080095c
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-04-12 18:14:48 +0200

    feat: courseinfo - exercise 1.2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,16 +1,12 @@
+import Part from './Part'
+
 const Content = (props) => {
     return (
-      <div>
-        <p>
-            {props.parts[0].name} {props.parts[0].exercises}
-        </p>
-        <p>
-            {props.parts[1].name} {props.parts[1].exercises}
-        </p>
-        <p>
-            {props.parts[2].name} {props.parts[2].exercises}
-        </p>
-      </div>
+        <div>
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
+        </div>
     )
   }
   
