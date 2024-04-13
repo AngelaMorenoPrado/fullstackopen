eead653f5c8a41f8fa8ecebb5d531ba1c7202295
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-04-13 18:38:32 +0200

    feat: make changes for exercise 1.9 step 4

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,4 +1,13 @@
 const Statistics = (props) => {
+    const isFeedbackMissing = props.values.slice(0, 3).every(item => item.value === 0)
+    if (isFeedbackMissing) {
+        return (
+            <p>
+                No feedback given
+            </p>
+        )
+    }
+
     const statistics = props.values.map((item, index) => {
         return (
             <p key={index}>
