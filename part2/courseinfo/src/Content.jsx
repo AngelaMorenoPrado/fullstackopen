53876d89a2cb75a549d4c7c952805e050d950f40
AngelaMorenoPrado <angelamorenoprado@gmail.com>
2024-04-14 15:14:14 +0200

    feat: implementation of exercise 2.3

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -1,11 +1,18 @@
 import Part from './Part'
+import Total from './Total'
 
 const Content = ({ data }) => {
+
+    const total = data.reduce((acc, current) => {
+        return acc + current.exercises
+    }, 0)
+
     return (
         <>
             {data.map(note => {
                 return <Part data={note} key={note.id} />
             })}
+            <Total total={total} />
         </>
     )
 }
